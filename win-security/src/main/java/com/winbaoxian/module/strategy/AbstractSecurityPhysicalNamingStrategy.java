e5990b2a0ab3360cfe64067e9b2886cd14368d0b
dongxl <dxl@winbaoxian.com>
2018-11-16 15:03:16 +0800

    权限管理

--- win-security/src/main/java/com/winbaoxian/module/strategy/AbstractSecurityPhysicalNamingStrategy.java
+++ win-security/src/main/java/com/winbaoxian/module/strategy/AbstractSecurityPhysicalNamingStrategy.java
@@ -5,25 +5,35 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 
-public abstract class AbstractSecurityNamingStrategy extends SpringPhysicalNamingStrategy {
+public abstract class AbstractSecurityPhysicalNamingStrategy extends SpringPhysicalNamingStrategy {
 
     public static final String[] SECURITY_TABLE_LIST = {"USER", "ROLE", "RESOURCE", "USER_ROLE", "ROLE_RESOURCE"};
 
     @Override
     public Identifier toPhysicalTableName(Identifier name, JdbcEnvironment jdbcEnvironment) {
         String nameText = name.getText().toUpperCase();
-        String tablePrefixText = tablePrefix();
+        String tablePrefixText = tablePrefix().toUpperCase();
+        Identifier newName = name;
         if (StringUtils.isNotBlank(tablePrefixText)) {
             for (String securityTableName : SECURITY_TABLE_LIST) {
                 if (securityTableName.equals(nameText)) {
-                    Identifier newName = new Identifier(tablePrefixText + "_" + name.getText(), name.isQuoted());
-                    return super.toPhysicalTableName(newName, jdbcEnvironment);
+                    newName = new Identifier(tablePrefixText + "_" + name.getText(), name.isQuoted());
+                    break;
                 }
             }
         }
-        return super.toPhysicalTableName(name, jdbcEnvironment);
+        if (isUnderscoreRequired()) {
+            return super.toPhysicalTableName(newName, jdbcEnvironment);
+        } else {
+            return newName;
+        }
     }
 
     public abstract String tablePrefix();
+
+    public boolean isUnderscoreRequired() {
+        return true;
+    }
+
 
 }
---
+++ win-security/src/main/resources/sercurity.sql
@@ -0,0 +1,103 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : test
+Source Server Version : 50634
+Source Host           : testwinbx.mysql.rds.aliyuncs.com:3306
+Source Database       : citymanager
+
+Target Server Type    : MYSQL
+Target Server Version : 50634
+File Encoding         : 65001
+
+Date: 2018-11-16 14:55:52
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for resource
+-- ----------------------------
+CREATE TABLE `{prefix}_resource` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `name` varchar(64) NOT NULL COMMENT '资源名称',
+  `code` varchar(64) NOT NULL DEFAULT '' COMMENT '编码',
+  `global_code` varchar(1024) DEFAULT '' COMMENT '全局编码, 格式: 系统code.菜单code.thisCode',
+  `value` varchar(100) DEFAULT NULL COMMENT '资源路径',
+  `description` varchar(255) DEFAULT NULL COMMENT '资源介绍',
+  `icon` varchar(32) DEFAULT NULL COMMENT '资源图标',
+  `pid` bigint(20) DEFAULT '0' COMMENT '父级资源id',
+  `seq` int(5) NOT NULL DEFAULT '0' COMMENT '排序',
+  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态，0：失效  1：有效',
+  `resource_type` tinyint(2) NOT NULL DEFAULT '0' COMMENT '资源类别, 0:无特别作用，1:菜单，2:子页面，3:按钮, 4:页面自定义变量',
+  `deleted` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资源';
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+CREATE TABLE `{prefix}_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `name` varchar(64) NOT NULL COMMENT '角色名',
+  `description` varchar(255) DEFAULT NULL COMMENT '简介',
+  `seq` int(5) NOT NULL DEFAULT '0' COMMENT '排序号',
+  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态，0：失效  1：有效',
+  `deleted` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色';
+
+-- ----------------------------
+-- Table structure for role_resource
+-- ----------------------------
+CREATE TABLE `{prefix}_role_resource` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色id',
+  `resource_id` bigint(20) NOT NULL COMMENT '资源id',
+  PRIMARY KEY (`id`),
+  KEY `idx_role_resource_ids` (`role_id`,`resource_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色资源';
+
+-- ----------------------------
+-- Table structure for sys_log
+-- ----------------------------
+CREATE TABLE `{prefix}_sys_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `user_name` varchar(255) DEFAULT NULL COMMENT '登陆名',
+  `role_name` varchar(255) DEFAULT NULL COMMENT '角色名',
+  `opt_content` varchar(1024) DEFAULT NULL COMMENT '内容',
+  `client_ip` varchar(255) DEFAULT NULL COMMENT '客户端ip',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统日志';
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+CREATE TABLE `{prefix}_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `user_name` varchar(64) NOT NULL COMMENT '登陆名',
+  `name` varchar(64) DEFAULT NULL COMMENT '用户名',
+  `mobile` varchar(20) DEFAULT NULL COMMENT '手机号',
+  `status` tinyint(2) NOT NULL DEFAULT '0' COMMENT '状态，0：失效  1：有效',
+  `deleted` bit(1) NOT NULL DEFAULT b'0',
+  PRIMARY KEY (`id`),
+  KEY `IDx_user_login_name` (`user_name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户';
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+CREATE TABLE `{prefix}_user_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `user_id` bigint(20) NOT NULL COMMENT '用户id',
+  `role_id` bigint(20) NOT NULL COMMENT '角色id',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_role_ids` (`user_id`,`role_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户角色';
