d7668204614c78c5f253294e0d38b32a8fdd1859
dongxl <dxl@winbaoxian.com>
2018-12-11 13:41:29 +0800

    winSecurity优化  resource去重

--- win-security/src/main/java/com/winbaoxian/module/security/repository/WinSecurityResourceRepository.java
+++ win-security/src/main/java/com/winbaoxian/module/security/repository/WinSecurityResourceRepository.java
@@ -19,7 +19,7 @@ public interface WinSecurityResourceRepository extends JpaRepository<WinSecurity
 
     List<WinSecurityResourceEntity> findByPidAndDeletedFalse(Long pid);
 
-    @Query("select a from WinSecurityResourceEntity a,WinSecurityRoleResourceEntity b,WinSecurityUserRoleEntity c,WinSecurityBaseRoleEntity d WHERE a.id= b.resourceId and b.roleId = c.roleId and b.roleId=d.id and c.userId=?1 and a.deleted=false and a.status=1 and d.status=1")
+    @Query("select distinct a from WinSecurityResourceEntity a,WinSecurityRoleResourceEntity b,WinSecurityUserRoleEntity c,WinSecurityBaseRoleEntity d WHERE a.id= b.resourceId and b.roleId = c.roleId and b.roleId=d.id and c.userId=?1 and a.deleted=false and a.status=1 and d.status=1")
     List<WinSecurityResourceEntity> getValidResourceListByUserId(Long userId);
 
 }
